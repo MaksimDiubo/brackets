5096eb492c1e4fae58033d19357b6cd9d6d1c036
Maksim Diubo <diubomaksim@gmail.com>
2020-03-06 18:36:06 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  
+  
+for (let i = 0; i < str.length; i++) {
+    stack.push(str[i])
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        if (bracketsConfig[j][0] === stack[stack.length - 2] && bracketsConfig[j][1] === str[i]) {
+            stack.pop();
+            stack.pop();  
+        }
+    }
+}
+
+return stack.length ? false : true;
+
 }
